0c742676a0a1d278b404f1cdde0b0acd8edb75bc
kaoussi <kaoussi.souhail@gmail.com>
2017-03-17 19:49:34 +0000

    added react and webpack

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
 var Hello = React.createClass({
   render: function() {
-    return React.createElement('h3', null, "Hello React");
+    return React.createElement("h3", null, "Hello Webpack!");
   }
 });
 
-ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
+ReactDOM.render(React.createElement(Hello), document.getElementById('main'));
